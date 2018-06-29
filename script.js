1f3f19265ad7c8b75bfb2c3b933ceef240f45d4d
Funmilayo Miracle Oladigbolu <miraclefunmilayooladigbolu@gmail.com>
2018-06-29 11:05:04 +0100

    Adding index.html

---
+++ script.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+var height, width, color;
+
+$('#sizePicker').submit(function(event){
+	event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWeight').val();
+	makeGrid(height, width);
+	// console.log('Height:' + height + 'and width:' + width);
+});
+
+function makeGrid(x, y){
+	$('tr').remove();
+		for(var i = 1; i <= x; i++){
+	$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+		for(var j = 1; j <= y; j++){
+	$('#table' + i).append('<td></td>');
+
+		}
+	}
+	//add color to cell
+	
+	$('td').click(function addColor(){
+		color = $('#colorPicker').val();
+
+		if($(this).attr('style')){
+			$(this).removeAttr('style')
+		}else{
+			$(this).attr('style','background-color:' + color);
+		}
+	});
+}
